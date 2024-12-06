aee06f6775a1d7e34342ad25f158069b319dd53c
MatteoNeri99 <matteolavoro99@gmail.com>
2024-12-06 13:01:42 +0100

    metodo prepare ed execute per rendere più veloce e sicuro il nostro codice

---
+++ 6_PREPARED/insert.php
@@ -0,0 +1,87 @@
+<?php 
+
+// prepared è un metodo che ci permette di scrivere una query e poi eseguirla quando ci serve tramite execute
+
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$nameDB = 'tutorial_db';
+
+
+$connessione = new mysqli($host, $username, $password, $nameDB );
+
+if ($connessione === false){
+    die('Problema nella connesione con il db: ' .  $connessione->error . '<br>');
+}
+
+echo 'connessione al db avvenuta con successo';
+
+// creo la query da salvare con il valori da inserire come punto di domanda in modo poi da andarli cambiare solo quelli quando la queri verrà inserita
+$sql = "INSERT INTO persone (nome, cognome, email) VALUES (?, ?, ?)";
+
+
+// preparo la query da inviare con il metodo prepare
+if($statement= $connessione->prepare($sql)){
+
+    // vado a cambiare i punti di domanda con i dati da inserire (il primo valore è il tipo di dato cha va inserito quindi in questo caso 3 stringhe e quindi 'sss')
+    $statement->bind_param('sss', $nome, $cognome, $email);
+
+    $nome = $_POST['nome'];
+    $cognome = $_POST['cognome'];
+    $email = $_POST['email'];
+
+    // eseugo la query
+    $statement->execute();
+
+    echo 'i record sono stati inseriti';
+
+}else{
+
+    echo 'problema nell\'inserimento dati';
+}
+
+$statement->close();
+$connessione->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
